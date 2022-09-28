8ae360de051a2b56c2658bc933158934929ae2d5
ansonant0ny <109643952+ansonant0ny@users.noreply.github.com>
2022-09-28 11:47:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
 from typing import List
 
 def insertionSort(array) -> List[int]:
-  for i in range(1,len(array)):
-      key = array[n]
-      num = n-1
-      while num >= 0 and key<array[num]:
-        num -= 1
-      array[num+1] = key
-  return array
+    for y in range(len(array)):
+       for x in range(1,len(array)):
+          j=x-1
+          while(array[x-1]>array[x] and j>=0):
+             array[x],array[x-1]=array[x-1],array[x]
+             j-=1
+    return array
 
-# data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -17,4 +16,4 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(insertionSort(data))
+print(insertionSort(data)
